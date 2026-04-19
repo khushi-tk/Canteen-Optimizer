6649eb7fbed6dc424a10d41971f2b8626ea1aafc
Triporna Karmakar <karmakartriporna@gmail.com>
2026-04-19 17:34:52 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1>🍽️ Smart Canteen Optimizer</h1>
+        <p>Let's go segmentation fault!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
